8138f8a90e10698a1a9992ad1d7580a4249137a6
pmint <pmint@users.noreply.github.com>
2020-03-22 23:32:54 +0900

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,36 @@
+#!/usr/bin/env ruby
+
+# A number of discharged COVID-19 patients in Japan from NHK(Japan Broadcasting Corporation)
+# from ScraperWiki
+
+require 'scraperwiki'
+require 'nokogiri'
+
+# Saving data:
+# unique_keys = [ 'id' ]
+# data = { 'id'=>12, 'name'=>'violet', 'age'=> 7 }
+# ScraperWiki.save_sqlite(unique_keys, data)
+
+
+
+url = "https://www3.nhk.or.jp/news/special/coronavirus/module/mod_site-info.html"
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset
+  f.read
+end
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.xpath('//*[@class="total"]//*[@class="tbody-td"][4]/text()').text =~ /(\d+)人/
+p $1
+ScraperWiki.save_sqlite(['id'], {
+    'id' => Time.now.to_i,
+    'number_blob' => $1,
+    'time_blob' => Time.now.getlocal('+09:00').to_s
+})
+
+
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
